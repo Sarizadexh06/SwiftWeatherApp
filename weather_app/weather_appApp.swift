94bd9cb9f5fb838b85b6a7dcedb3fb65689d9028
Pasha Sarizada <pashasarizada@gmail.com>
2025-02-13 16:33:18 +0300

    Initial Commit

---
+++ weather_app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather_app
+//
+//  Created by TEST on 13.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
