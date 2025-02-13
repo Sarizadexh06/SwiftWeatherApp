2ddfed0894c38b616585193574bf8b29d1e18772
Pasha Sarizada <pashasarizada@gmail.com>
2025-02-13 16:54:18 +0300

    weather app

---
+++ weather_app/View/WeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherView.swift
+//  weather_app
+//
+//  Created by TEST on 13.02.2025.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
---
+++ weather_app/ViewModel/WeatherViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherViewModel.swift
+//  weather_app
+//
+//  Created by TEST on 13.02.2025.
+//
+
+import Foundation
